d9538b082b14f661c72b5b3606ad3c26435cffd6
Roman Plansky <roman.plansky@seyfor.com>
2025-04-26 03:37:56 +0200

    Component

---
+++ components/my-header.js
@@ -0,0 +1,16 @@
+class MyHeader extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+        <header>
+          <h1>Můj E-Shop</h1>
+          <nav>
+            <a href="index.html">Domů</a>
+            <a href="produkty.html">Produkty</a>
+            <a href="kosik.html">Košík</a>
+          </nav>
+        </header>
+      `;
+    }
+  }
+
+  customElements.define('my-header', MyHeader);
